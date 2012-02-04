ebc4d45462fff983f49fbb6bf8e6e5038e4c885d
jchiu09 <jchiu09@gmail.com>
2012-02-04 18:04:33 +0000

    Changed main.xml and renamed TamagotchiProjectActivity to Main

--- Tamagotchi Project/src/org/tamaproject/Main.java
+++ Tamagotchi Project/src/org/tamaproject/Main.java
@@ -3,7 +3,7 @@ package org.tamaproject;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class TamagotchiProjectActivity extends Activity {
+public class Main extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
