414e0ddcf47e571c85892af7f92ebedefd19f630
jchiu09@gmail.com <jchiu09@gmail.com@a30f3c6d-67b0-a8cc-a227-7585017cefeb>
2012-02-17 03:40:28 +0000

    - Added attributes and methods to Tamagotchi

--- trunk/Tamagotchi Project/src/com/tamaproject/gameobjects/Tamagotchi.java
+++ trunk/Tamagotchi Project/src/com/tamaproject/gameobjects/Tamagotchi.java
@@ -6,14 +6,124 @@ import com.tamaproject.GameObject;
 
 public class Tamagotchi extends GameObject
 {
-    int currentHealth, maxHealth;
-    int hunger;
-    int currentXP, maxXP;
-    int poop;
-    int battleLevel;
+    private int currentHealth, maxHealth;
+    private int currentHunger, maxHunger;
+    private int currentXP, maxXP;
+    private int currentSickness, maxSickness;
+    private int poop;
+    private int battleLevel;
+    private int status;
+
+    public static int ALIVE = 1, DEAD = 0;
 
     public Tamagotchi(Bitmap bitmap, int x, int y)
     {
 	super(bitmap, x, y);
+	setDefault();
+    }
+
+    private void setDefault()
+    {
+	this.currentHealth = 100;
+	this.maxHealth = 150;
+	this.currentHunger = 10;
+	this.maxHunger = 50;
+	this.currentXP = 10;
+	this.maxXP = 50;
+	this.currentSickness = 10;
+	this.maxSickness = 50;
+	this.poop = 6;
+	this.battleLevel = 5;
+	this.status = Tamagotchi.ALIVE;
+    }
+
+    public Tamagotchi(Bitmap bitmap, int x, int y, int currentHealth, int maxHealth,
+	    int currentHunger, int maxHunger, int currentXP, int maxXP, int currentSickness,
+	    int maxSickness, int poop, int battleLevel, int status)
+    {
+	super(bitmap, x, y);
+	this.currentHealth = currentHealth;
+	this.maxHealth = maxHealth;
+	this.currentHunger = currentHunger;
+	this.maxHunger = maxHunger;
+	this.currentXP = currentXP;
+	this.maxXP = maxXP;
+	this.currentSickness = currentSickness;
+	this.maxSickness = maxSickness;
+	this.poop = poop;
+	this.battleLevel = battleLevel;
+	this.status = status;
+    }
+
+    public boolean applyItem(Item item)
+    {
+	this.currentHealth += item.getHealth();
+	this.currentHunger += item.getHunger();
+	this.currentSickness += item.getSickness();
+	this.currentXP += item.getXp();
+
+	checkStats();
+	return true;
+    }
+
+    // makes sure that the values are legit
+    private void checkStats()
+    {
+	if (this.currentHealth > this.maxHealth)
+	{
+	    this.currentHealth = this.maxHealth;
+	}
+
+	if (this.currentHunger < 0)
+	{
+	    this.currentHunger = 0;
+	}
+
+	if (this.currentSickness < 0)
+	{
+	    this.currentSickness = 0;
+	}
+
+	// check if tama leveled up
+	levelUp();
+
+	// check if tama is dead
+	isDead();
+    }
+
+    public boolean isDead()
+    {
+	if (status == Tamagotchi.DEAD)
+	{
+	    return true;
+	}
+	else if (currentHunger > maxHunger || currentSickness > maxSickness || currentHealth < 0)
+	{
+	    status = Tamagotchi.DEAD;
+	    return true;
+	}
+
+	return false;
+    }
+
+    private boolean levelUp()
+    {
+	if (this.currentXP > this.maxXP)
+	{
+	    this.battleLevel++;
+
+	    this.currentXP = this.maxXP - this.currentXP;
+	    this.maxXP *= 2;
+
+	    this.maxHealth += this.maxHealth / 2;
+	    this.currentHealth = this.maxHealth;
+
+	    this.maxHunger += this.maxHunger / 4;
+
+	    this.maxSickness += this.maxSickness / 4;
+
+	    return true;
+	}
+	return false;
     }
 }
