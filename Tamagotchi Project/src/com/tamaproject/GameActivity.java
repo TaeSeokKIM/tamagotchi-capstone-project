0b6386172eea176f3d474c80f629359a674724be
jchiu09 <jchiu09@gmail.com>
2012-02-19 00:35:06 +0000

    - Moved drawing the backpack rectangle to Backpack class

--- Tamagotchi Project/src/com/tamaproject/GameActivity.java
+++ Tamagotchi Project/src/com/tamaproject/GameActivity.java
@@ -1,15 +1,20 @@
 package com.tamaproject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class GameActivity extends Activity
 {
     private static final String TAG = GameActivity.class.getSimpleName();
     private GameView gv;
+    private static final int CONFIRM_ENDGAME = 0;
 
     public void onCreate(Bundle savedInstanceState)
     {
@@ -33,6 +38,40 @@ public class GameActivity extends Activity
 	Log.d(TAG, "Stopping...");
 	super.onStop();
 	finish();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id)
+    {
+	switch (id)
+	{
+	case GameActivity.CONFIRM_ENDGAME:
+	    AlertDialog.Builder builder2 = new AlertDialog.Builder(this);
+	    builder2.setTitle("End Game");
+	    builder2.setIcon(android.R.drawable.btn_star);
+	    builder2.setMessage("Are you sure you want to quit?");
+	    builder2.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener()
+	    {
+		public void onClick(DialogInterface dialog, int which)
+		{
+		    finish();
+		    return;
+		}
+	    });
+
+	    builder2.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener()
+	    {
+		public void onClick(DialogInterface dialog, int which)
+		{
+		    //Toast.makeText(getApplicationContext(), "Clicked Cancel!", Toast.LENGTH_SHORT).show();
+		    return;
+		}
+	    });
+
+	    return builder2.create();
+	}
+
+	return null;
     }
 
 }
