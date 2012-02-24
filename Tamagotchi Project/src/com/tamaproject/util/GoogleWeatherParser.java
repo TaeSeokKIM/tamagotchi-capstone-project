b29c4864c59e407caaab653f71cba7b12ccc6d7c
jchiu09 <jchiu09@gmail.com>
2012-02-24 02:59:30 +0000

    - Added GPS and weather retrieval

---
+++ Tamagotchi Project/src/com/tamaproject/util/GoogleWeatherParser.java
@@ -0,0 +1,105 @@
+package com.tamaproject.util;
+
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import com.tamaproject.weather.*;
+
+public class GoogleWeatherParser
+{
+    private static Document dom;
+
+    public static CurrentConditions getCurrentConditions(String file)
+    {
+	parseXMLFile(file);
+	if (dom == null)
+	    return null;
+	else
+	    return parseDocument();
+    }
+
+    private static void parseXMLFile(String file)
+    {
+	// System.out.println("Parsing XML to DOM");
+	DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	try
+	{
+	    DocumentBuilder db = dbf.newDocumentBuilder();
+	    dom = db.parse(file);
+
+	} catch (ParserConfigurationException pce)
+	{
+	    pce.printStackTrace();
+	} catch (SAXException se)
+	{
+	    se.printStackTrace();
+	} catch (IOException ioe)
+	{
+	    ioe.printStackTrace();
+	}
+    }
+
+    private static CurrentConditions parseDocument()
+    {
+	Element docEle = dom.getDocumentElement();
+	CurrentConditions cc = new CurrentConditions();
+	NodeList nl = docEle.getElementsByTagName("current_conditions");
+	if (nl != null && nl.getLength() > 0)
+	{
+	    for (int i = 0; i < nl.getLength(); i++)
+	    {
+		Element el = (Element) nl.item(i);
+		cc = getCC(el);
+	    }
+	}
+	return cc;
+    }
+
+    private static CurrentConditions getCC(Element e)
+    {
+	CurrentConditions cc = new CurrentConditions();
+	NodeList nl = e.getChildNodes();
+	if (nl != null && nl.getLength() > 0)
+	{
+	    for (int i = 0; i < nl.getLength(); i++)
+	    {
+		Element el = (Element) nl.item(i);
+		if (el.getTagName().equalsIgnoreCase("condition"))
+		{
+		    cc.setCondition(el.getAttribute("data"));
+		}
+		else if (el.getTagName().equalsIgnoreCase("temp_f"))
+		{
+		    cc.setTempF(Integer.parseInt(el.getAttribute("data")));
+		}
+		else if (el.getTagName().equalsIgnoreCase("temp_c"))
+		{
+		    cc.setTempC(Integer.parseInt(el.getAttribute("data")));
+		}
+		else if (el.getTagName().equalsIgnoreCase("humidity"))
+		{
+		    cc.setHumidity(el.getAttribute("data"));
+		}
+		else if (el.getTagName().equalsIgnoreCase("icon"))
+		{
+		    cc.setIcon(el.getAttribute("data"));
+		}
+		else if (el.getTagName().equalsIgnoreCase("wind_condition"))
+		{
+		    cc.setWindCondition(el.getAttribute("data"));
+		}
+	    }
+	}
+	return cc;
+    }
+
+}
---
+++ Tamagotchi Project/src/com/tamaproject/weather/WeatherRetriever.java
@@ -0,0 +1,23 @@
+package com.tamaproject.weather;
+
+import com.tamaproject.*;
+import com.tamaproject.util.GoogleWeatherParser;
+
+public class WeatherRetriever
+{
+    private static final String XML_SOURCE = "http://www.google.com/ig/api?weather=,,,";
+
+    public static CurrentConditions getCurrentConditions(double latitude, double longitude)
+    {
+	latitude *= 1000000;
+	longitude *= 1000000;
+	int lat = (int) latitude;
+	int lon = (int) longitude;
+	
+	String s = XML_SOURCE + lat + "," + lon;
+	System.out.println(s);
+	
+	CurrentConditions cc = GoogleWeatherParser.getCurrentConditions(s);
+	return cc;
+    }
+}
