7a9d42dc853a3a4794238db68f7cbdab2ef841e1
jchiu09 <jchiu09@gmail.com>
2012-03-06 00:20:46 +0000

    - Added a protection field to Item to indicate weather or not it protects against something (rain, snow, etc) - Added a Protection class that holds the different types of protections possible - Added timer to increase sickness if tama not prepared for weather

---
+++ Tamagotchi Project/src/com/tamaproject/andengine/entity/Protection.java
@@ -0,0 +1,11 @@
+package com.tamaproject.andengine.entity;
+
+import com.tamaproject.util.Weather;
+
+public class Protection
+{
+    // All weather-related things are between 1-10
+    public static final int NONE = Weather.NONE, SNOW = Weather.SNOW, RAIN = Weather.RAIN;
+    
+    // Other protection will be 11-100
+}
