62476aaec0df46961189a774ac6a46cbb6068360
jchiu09 <jchiu09@gmail.com>
2012-03-08 21:22:40 +0000

    Added notifications

--- Tamagotchi Project/src/com/tamaproject/util/Weather.java
+++ Tamagotchi Project/src/com/tamaproject/util/Weather.java
@@ -5,5 +5,6 @@ public class Weather
     public static final int SNOW = 0;
     public static final int RAIN = 1;
     public static final int NONE = -1;
+    public static final int CLOUDY = 2;
 
 }
