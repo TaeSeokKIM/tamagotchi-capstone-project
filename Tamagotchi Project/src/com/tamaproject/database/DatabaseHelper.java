90545fbe33d775c67f19e83b5155d084e99aaa46
jchiu09 <jchiu09@gmail.com>
2012-04-03 01:31:35 +0000

    Added multiplayer
    - Players can connect and can tell which player number they are.

--- Tamagotchi Project/src/com/tamaproject/database/DatabaseHelper.java
+++ Tamagotchi Project/src/com/tamaproject/database/DatabaseHelper.java
@@ -44,35 +44,6 @@ public class DatabaseHelper extends SQLiteOpenHelper
 	this.context = ctx;
     }
 
-    /**
-     * Creates an empty database on the system and rewrites it with the database.
-     */
-    public void createDatabase() throws IOException
-    {
-	boolean dbExist = checkDatabase();
-
-	if (dbExist)
-	{
-	    System.out.println("Database Exists");
-	}
-	else
-	{
-	    /*
-	     * By calling this method and empty database will be created into the default system path of the application so we can overwrite the database with the application
-	     */
-
-	    this.getReadableDatabase();
-
-	    try
-	    {
-		copyDatabase();
-	    } catch (IOException e)
-	    {
-		e.printStackTrace();
-	    }
-	}
-    }
-
     public static void createDatabaseIfNotExists(Context context) throws IOException
     {
 	boolean createDb = false;
@@ -133,60 +104,6 @@ public class DatabaseHelper extends SQLiteOpenHelper
     public static SQLiteDatabase getStaticDb()
     {
 	return SQLiteDatabase.openDatabase(DB_PATH + DB_NAME, null, SQLiteDatabase.OPEN_READONLY);
-    }
-
-    /**
-     * Check if the database already exist to avoid re-copying the file each time the application is opened.
-     * 
-     * @return true if it exists, false if it doesn't
-     */
-    private boolean checkDatabase()
-    {
-	SQLiteDatabase checkDB = null;
-
-	try
-	{
-	    String path = DB_PATH + DB_NAME;
-	    checkDB = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READWRITE);
-	} catch (SQLiteException e)
-	{
-	    e.printStackTrace();
-	}
-
-	if (checkDB != null)
-	{
-	    checkDB.close();
-	}
-
-	return checkDB != null ? true : false;
-    }
-
-    /**
-     * Copies your database from the local assets-folder to the just created empty database in teh system folder, from where it can be accessed and handled. This is done by transferring bytestream.
-     */
-    private void copyDatabase() throws IOException
-    {
-	/* Open your local db as the input stream */
-	InputStream input = context.getAssets().open(DB_NAME);
-
-	/* Path to the just create db */
-	String outFileName = DB_PATH + DB_NAME;
-
-	/* Open the empty db as the output stream */
-	OutputStream output = new FileOutputStream(outFileName);
-
-	/* Transfer bytes from the inputfile to outputfile */
-	byte[] buffer = new byte[1024];
-	int length;
-	while ((length = input.read(buffer)) > 0)
-	{
-	    output.write(buffer, 0, length);
-	}
-
-	/* Close the Streams */
-	output.flush();
-	output.close();
-	input.close();
     }
 
     /**
---
+++ Tamagotchi Project/src/com/tamaproject/multiplayer/MultiplayerConstants.java
@@ -0,0 +1,11 @@
+package com.tamaproject.multiplayer;
+
+public class MultiplayerConstants
+{
+    public static final String XP_GAIN = "Experience Gained";
+    public static final String HEALTH = "Health";
+    public static final String MAX_HEALTH = "Max Health";
+    public static final String ALIVE_OR_DEAD = "Alive or Dead";
+    public static final String BATTLE_LEVEL = "Battle Level";
+
+}
