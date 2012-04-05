0475a19719defcc48c67926f5da876fd21eaeefe
jchiu09@gmail.com <jchiu09@gmail.com@a30f3c6d-67b0-a8cc-a227-7585017cefeb>
2012-04-05 05:12:56 +0000

    Added bullets

---
+++ trunk/Tamagotchi Project/src/com/tamaproject/multiplayer/BulletPool.java
@@ -0,0 +1,50 @@
+package com.tamaproject.multiplayer;
+
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.util.pool.GenericPool;
+
+public class BulletPool extends GenericPool<Sprite>
+{
+    private TextureRegion mTextureRegion;
+
+    public BulletPool(TextureRegion pTextureRegion)
+    {
+	if (pTextureRegion == null)
+	{
+	    // Need to be able to create a Sprite so the Pool needs to have a TextureRegion
+	    throw new IllegalArgumentException("The texture region must not be NULL");
+	}
+	mTextureRegion = pTextureRegion;
+    }
+
+    /**
+     * Called when a Bullet is required but there isn't one in the pool
+     */
+    @Override
+    protected Sprite onAllocatePoolItem()
+    {
+	return new Sprite(0, 0, mTextureRegion);
+    }
+
+    /**
+     * Called when a Bullet is sent to the pool
+     */
+    @Override
+    protected void onHandleRecycleItem(final Sprite pBullet)
+    {
+	pBullet.setIgnoreUpdate(true);
+	pBullet.setVisible(false);
+    }
+
+    /**
+     * Called just before a Bullet is returned to the caller, this is where you write your initialize code i.e. set location, rotation, etc.
+     */
+    @Override
+    protected void onHandleObtainItem(final Sprite pBullet)
+    {
+	pBullet.reset();
+	pBullet.detachSelf();
+	pBullet.clearUpdateHandlers();
+    }
+}
