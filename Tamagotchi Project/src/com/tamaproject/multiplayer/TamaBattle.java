00dd129c62665ea87cd92d96ab1cf34e57752032
jchiu09 <jchiu09@gmail.com>
2012-04-12 20:19:55 +0000

    Fixed up MiniGame.java but there's still a problem somewhere

--- Tamagotchi Project/src/com/tamaproject/multiplayer/TamaBattle.java
+++ Tamagotchi Project/src/com/tamaproject/multiplayer/TamaBattle.java
@@ -71,7 +71,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.util.SparseArray;
-import android.view.KeyEvent;
 import android.widget.EditText;
 import android.widget.Toast;
 
