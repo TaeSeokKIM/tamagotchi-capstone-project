df282da5dd7a42a35020dd8aa131809e0e9c9404
jchiu09@gmail.com <jchiu09@gmail.com@a30f3c6d-67b0-a8cc-a227-7585017cefeb>
2012-04-16 00:26:29 +0000

    Added some more items Fixed up item store, now supports multiple pages of items

--- trunk/Tamagotchi Project/src/com/tamaproject/itemstore/ItemStore.java
+++ trunk/Tamagotchi Project/src/com/tamaproject/itemstore/ItemStore.java
@@ -7,12 +7,17 @@ import java.util.LinkedList;
 
 import org.anddev.andengine.engine.Engine;
 import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.handler.IUpdateHandler;
 import org.anddev.andengine.engine.options.EngineOptions;
 import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.anddev.andengine.entity.Entity;
+import org.anddev.andengine.entity.modifier.LoopEntityModifier;
+import org.anddev.andengine.entity.modifier.ScaleModifier;
+import org.anddev.andengine.entity.modifier.SequenceEntityModifier;
 import org.anddev.andengine.entity.primitive.Rectangle;
 import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.entity.text.ChangeableText;
 import org.anddev.andengine.entity.text.Text;
@@ -60,6 +65,9 @@ public class ItemStore extends BaseAndEngineGame
     private Backpack backpack;
     private int money = 0;
     private Item itemToPurchase;
+    private ArrayList<Entity> itemPages = new ArrayList<Entity>();
+    private int currentPage = 0;
+    private ChangeableText titleText;
 
     @Override
     public void onLoadComplete()
@@ -158,71 +166,20 @@ public class ItemStore extends BaseAndEngineGame
 	this.money = intent.getIntExtra("money", 0);
 
 	this.mScene = new Scene();
+	this.mScene.setBackground(new ColorBackground(247 / 255f, 233 / 255f, 103 / 255f));
 	this.topLayer = new Entity();
 	this.bottomLayer = new Entity();
 	this.mScene.attachChild(bottomLayer);
 	this.mScene.attachChild(topLayer);
 
 	this.loadBackpack();
+	this.loadStoreItems();
 	this.loadItemDescriptionBox();
+	this.loadTopBar();
+	this.loadBottomBar();
 
-	ArrayList<Item> allItems = dbHelper.getAllItems(listTR);
-	LinkedList<Rectangle> itemBoxes = new LinkedList<Rectangle>();
-	for (final Item item : allItems)
-	{
-	    final Rectangle itemBox = new Rectangle(5, pTopBound, cameraWidth - 10, item.getHeight() + 4)
-	    {
-		@Override
-		public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
-			final float pTouchAreaLocalX, final float pTouchAreaLocalY)
-		{
-		    if (pSceneTouchEvent.isActionDown())
-		    {
-			showItemDescription(item);
-			return true;
-		    }
-		    return false;
-		}
-	    };
-	    mScene.registerTouchArea(itemBox);
-	    item.setPosition(2, 2);
-	    itemBox.attachChild(item);
-	    itemBox.setColor(1, 0, 0);
-
-	    final Text itemText = new Text(0, 0, mSmallFont, item.getName() + ": $" + item.getPrice());
-	    itemText.setPosition(item.getWidth() + 5, item.getHeight() / 2 - itemText.getHeight() / 2);
-	    item.attachChild(itemText);
-
-	    final Text buyText = new Text(0, 0, mSmallFont, "Buy");
-	    final Rectangle buyButton = new Rectangle(0, 0, buyText.getWidth() + 50, itemBox.getHeight())
-	    {
-		@Override
-		public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
-			final float pTouchAreaLocalX, final float pTouchAreaLocalY)
-		{
-		    if (pSceneTouchEvent.isActionDown())
-		    {
-			itemToPurchase = item;
-			showDialog(ItemStore.CONFIRM_PURCHASE);
-			return true;
-		    }
-		    return false;
-		}
-	    };
-	    mScene.registerTouchArea(buyButton);
-	    buyButton.setColor(0, 1, 0);
-	    buyText.setPosition(25, buyButton.getHeight() / 2 - buyText.getHeight() / 2);
-	    buyButton.attachChild(buyText);
-	    // buyButton.setPosition(itemBox.getWidth() - buyButton.getWidth() - 5, 2);
-	    itemBox.setWidth(cameraWidth - buyButton.getWidth() - 2);
-	    buyButton.setPosition(itemBox.getWidth(), 0);
-	    itemBox.attachChild(buyButton);
-	    if (!itemBoxes.isEmpty())
-		itemBox.setPosition(5, itemBoxes.getLast().getY() + itemBoxes.getLast().getHeight() + 5);
-	    itemBoxes.add(itemBox);
-	    bottomLayer.attachChild(itemBox);
-	}
 	mScene.setTouchAreaBindingEnabled(true);
+	mScene.setOnAreaTouchTraversalFrontToBack();
 	return mScene;
     }
 
@@ -240,6 +197,307 @@ public class ItemStore extends BaseAndEngineGame
 
     }
 
+    private void loadTopBar()
+    {
+	final Rectangle topRect = new Rectangle(0, 0, cameraWidth, pTopBound);
+	topRect.setColor(169 / 255f, 207 / 255f, 84 / 255f);
+	topLayer.attachChild(topRect);
+
+	titleText = new ChangeableText(0, 0, mFont, "Item Store", 25);
+	titleText.setPosition(40, topRect.getHeight() / 2 - titleText.getHeight() / 2);
+	topRect.attachChild(titleText);
+
+	final Sprite shopIcon = new Sprite(0, 0, listTR.get("shop_big.png"));
+	shopIcon.setPosition(cameraWidth - shopIcon.getWidth() - 40, pTopBound / 2 - shopIcon.getHeight() / 2);
+	shopIcon.registerEntityModifier(new LoopEntityModifier(new SequenceEntityModifier(new ScaleModifier(1f, 1, 1.1f), new ScaleModifier(1f, 1.1f, 1))));
+	topRect.attachChild(shopIcon);
+    }
+
+    private void loadBottomBar()
+    {
+	final Rectangle bottomRect = new Rectangle(0, pBottomBound, cameraWidth, cameraHeight - pBottomBound);
+	bottomRect.setColor(169 / 255f, 207 / 255f, 84 / 255f);
+	topLayer.attachChild(bottomRect);
+
+	final ChangeableText moneyText = new ChangeableText(0, 0, mFont, "Money: $" + money, 40);
+	moneyText.setPosition(40, bottomRect.getHeight() / 2 - moneyText.getHeight() / 2);
+	bottomRect.attachChild(moneyText);
+	moneyText.registerUpdateHandler(new IUpdateHandler()
+	{
+
+	    @Override
+	    public void reset()
+	    {
+
+	    }
+
+	    @Override
+	    public void onUpdate(float arg0)
+	    {
+		moneyText.setText("Money: $" + money);
+	    }
+	});
+
+	final Sprite rightArrowSprite = new Sprite(0, 0, listTR.get("right_icon.png"))
+	{
+	    private boolean touched = false;
+	    private boolean moved = false;
+
+	    @Override
+	    public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
+		    final float pTouchAreaLocalX, final float pTouchAreaLocalY)
+	    {
+		if (!this.getParent().isVisible())
+		    return false;
+
+		if (pSceneTouchEvent.isActionDown())
+		{
+		    touched = true;
+		}
+		else if (pSceneTouchEvent.isActionMove())
+		{
+		    moved = true;
+		}
+		else if (pSceneTouchEvent.isActionUp())
+		{
+		    if (touched)
+		    {
+			if (currentPage < (itemPages.size() - 1))
+			{
+			    itemPages.get(currentPage).setVisible(false);
+			    itemPages.get(++currentPage).setVisible(true);
+			}
+		    }
+
+		    touched = false;
+		}
+		return true;
+	    }
+	};
+	rightArrowSprite.setPosition(bottomRect.getWidth() - rightArrowSprite.getWidth() - 20, bottomRect.getHeight() / 2 - rightArrowSprite.getHeight() / 2);
+	rightArrowSprite.registerUpdateHandler(new IUpdateHandler()
+	{
+
+	    @Override
+	    public void reset()
+	    {
+
+	    }
+
+	    @Override
+	    public void onUpdate(float arg0)
+	    {
+		if (currentPage < (itemPages.size() - 1))
+		{
+		    rightArrowSprite.setVisible(true);
+		}
+		else
+		{
+		    rightArrowSprite.setVisible(false);
+		}
+	    }
+	});
+	mScene.registerTouchArea(rightArrowSprite);
+	rightArrowSprite.setVisible(false);
+	bottomRect.attachChild(rightArrowSprite);
+
+	final Sprite leftArrowSprite = new Sprite(0, 0, listTR.get("left_icon.png"))
+	{
+	    private boolean touched = false;
+	    private boolean moved = false;
+
+	    @Override
+	    public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
+		    final float pTouchAreaLocalX, final float pTouchAreaLocalY)
+	    {
+		if (!this.getParent().isVisible())
+		    return false;
+
+		if (pSceneTouchEvent.isActionDown())
+		{
+		    touched = true;
+		}
+		else if (pSceneTouchEvent.isActionMove())
+		{
+		    moved = true;
+		}
+		else if (pSceneTouchEvent.isActionUp())
+		{
+		    if (touched)
+		    {
+			if (currentPage > 0)
+			{
+			    itemPages.get(currentPage).setVisible(false);
+			    itemPages.get(--currentPage).setVisible(true);
+			}
+		    }
+
+		    touched = false;
+		}
+		return true;
+	    }
+	};
+	leftArrowSprite.setPosition(rightArrowSprite.getX() - leftArrowSprite.getWidth() - 20, rightArrowSprite.getY());
+	leftArrowSprite.registerUpdateHandler(new IUpdateHandler()
+	{
+
+	    @Override
+	    public void reset()
+	    {
+
+	    }
+
+	    @Override
+	    public void onUpdate(float arg0)
+	    {
+		if (currentPage > 0)
+		{
+		    leftArrowSprite.setVisible(true);
+		}
+		else
+		{
+		    leftArrowSprite.setVisible(false);
+		}
+	    }
+	});
+	mScene.registerTouchArea(leftArrowSprite);
+	leftArrowSprite.setVisible(false);
+	bottomRect.attachChild(leftArrowSprite);
+    }
+
+    private void loadStoreItems()
+    {
+	ArrayList<Item> allItems = dbHelper.getAllItems(listTR);
+	float boxHeight = (pBottomBound - pTopBound) / 10;
+	int spacing = 5;
+	int counter = 0;
+	Entity currentLayer = new Entity();
+	for (final Item item : allItems)
+	{
+	    counter++;
+	    final Rectangle itemBox = new Rectangle(spacing, pTopBound + spacing, cameraWidth - 2 * spacing, boxHeight - spacing)
+	    {
+		private boolean touched = false;
+		private boolean moved = false;
+
+		@Override
+		public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
+			final float pTouchAreaLocalX, final float pTouchAreaLocalY)
+		{
+		    if (!this.getParent().isVisible())
+			return false;
+
+		    if (pSceneTouchEvent.isActionDown())
+		    {
+			touched = true;
+		    }
+		    else if (pSceneTouchEvent.isActionMove())
+		    {
+			moved = true;
+		    }
+		    else if (pSceneTouchEvent.isActionUp())
+		    {
+			if (touched)
+			    showItemDescription(item);
+
+			touched = false;
+		    }
+		    return true;
+		}
+	    };
+	    mScene.registerTouchArea(itemBox);
+	    item.setPosition(2, 2);
+	    itemBox.attachChild(item);
+	    itemBox.setColor(4 / 255f, 191 / 255f, 191 / 255f);
+
+	    final Text itemText = new Text(0, 0, mSmallFont, item.getName() + ": $" + item.getPrice());
+	    itemText.setPosition(item.getWidth() + 5, item.getHeight() / 2 - itemText.getHeight() / 2);
+	    item.attachChild(itemText);
+
+	    final Text buyText = new Text(0, 0, mSmallFont, "Buy");
+	    final Rectangle buyButton = new Rectangle(0, 0, buyText.getWidth() + 50, itemBox.getHeight())
+	    {
+
+		private boolean touched = false;
+		private boolean moved = false;
+
+		@Override
+		public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
+			final float pTouchAreaLocalX, final float pTouchAreaLocalY)
+		{
+		    if (!itemBox.getParent().isVisible())
+			return false;
+
+		    if (pSceneTouchEvent.isActionDown())
+		    {
+			touched = true;
+		    }
+		    else if (pSceneTouchEvent.isActionMove())
+		    {
+			moved = true;
+		    }
+		    else if (pSceneTouchEvent.isActionUp())
+		    {
+			if (touched)
+			{
+			    itemToPurchase = item;
+			    showDialog(ItemStore.CONFIRM_PURCHASE);
+			}
+			touched = false;
+		    }
+		    return true;
+		}
+	    };
+	    mScene.registerTouchArea(buyButton);
+	    buyButton.setColor(88 / 255f, 143 / 255f, 39 / 255f);
+	    buyText.setPosition(25, buyButton.getHeight() / 2 - buyText.getHeight() / 2);
+	    buyButton.attachChild(buyText);
+	    // buyButton.setPosition(itemBox.getWidth() - buyButton.getWidth() - 5, 2);
+	    itemBox.setWidth(cameraWidth - buyButton.getWidth() - 10);
+	    buyButton.setPosition(itemBox.getWidth(), 0);
+	    itemBox.attachChild(buyButton);
+	    if (currentLayer.getChildCount() > 0)
+		itemBox.setPosition(5, currentLayer.getLastChild().getY() + ((Rectangle) currentLayer.getLastChild()).getHeight() + 5);
+
+	    currentLayer.attachChild(itemBox);
+
+	    if (counter >= 10)
+	    {
+		currentLayer.setVisible(false);
+		itemPages.add(currentLayer);
+		bottomLayer.attachChild(currentLayer);
+		currentLayer = new Entity();
+		counter = 0;
+	    }
+	}
+
+	if (counter > 0 && counter < 10)
+	{
+	    currentLayer.setVisible(false);
+	    itemPages.add(currentLayer);
+	    bottomLayer.attachChild(currentLayer);
+	}
+
+	itemPages.get(0).setVisible(true);
+	currentPage = 0;
+
+	mScene.registerUpdateHandler(new IUpdateHandler()
+	{
+
+	    @Override
+	    public void reset()
+	    {
+
+	    }
+
+	    @Override
+	    public void onUpdate(float arg0)
+	    {
+		titleText.setText("Item Store  (" + (currentPage + 1) + "/" + itemPages.size() + ")");
+	    }
+	});
+    }
+
     private void loadItemDescriptionBox()
     {
 	/**
@@ -252,7 +510,10 @@ public class ItemStore extends BaseAndEngineGame
 		    final float pTouchAreaLocalX, final float pTouchAreaLocalY)
 	    {
 		if (this.isVisible())
+		{
+		    Debug.d("Touched item description box");
 		    return true;
+		}
 		else
 		    return false;
 	    }
@@ -260,6 +521,7 @@ public class ItemStore extends BaseAndEngineGame
 	this.itemDescriptionRect.setColor(0, 0, 0);
 	this.itemDescriptionRect.setAlpha(.8f);
 	this.itemDescriptionRect.setVisible(false);
+	mScene.registerTouchArea(itemDescriptionRect);
 	this.topLayer.attachChild(itemDescriptionRect);
 
 	/**
@@ -267,17 +529,29 @@ public class ItemStore extends BaseAndEngineGame
 	 */
 	final Sprite closeButton = new Sprite(this.itemDescriptionRect.getWidth() - listTR.get("close.png").getWidth(), 0, listTR.get("close.png"))
 	{
+	    private boolean touched = false;
+
 	    @Override
 	    public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
 		    final float pTouchAreaLocalX, final float pTouchAreaLocalY)
 	    {
-		if (this.getParent().isVisible())
-		{
-		    hideItemDescription();
-		    return true;
-		}
-		else
+		if (!this.getParent().isVisible())
 		    return false;
+
+		if (pSceneTouchEvent.isActionDown())
+		{
+		    Debug.d("Touched close button");
+		    hideItemDescription();
+		}
+		else if (pSceneTouchEvent.isActionMove())
+		{
+
+		}
+		else if (pSceneTouchEvent.isActionUp())
+		{
+
+		}
+		return true;
 	    }
 	};
 	this.itemDescriptionRect.attachChild(closeButton);
@@ -298,6 +572,7 @@ public class ItemStore extends BaseAndEngineGame
      */
     private void showItemDescription(Item i)
     {
+	this.itemDescriptionRect.setVisible(false);
 	String normalizedText = TextUtil.getNormalizedText(mSmallFont, i.getInfoWithPrice(), this.itemDescriptionRect.getWidth() - 20);
 	this.itemDesctiptionText.setText(normalizedText);
 	this.itemDescriptionRect.setHeight(this.itemDesctiptionText.getHeight());
@@ -329,7 +604,7 @@ public class ItemStore extends BaseAndEngineGame
 	    switch (id)
 	    {
 	    case ItemStore.CONFIRM_PURCHASE:
-		builder2.setTitle("Give Item");
+		builder2.setTitle("Confirm Purchase");
 		builder2.setIcon(android.R.drawable.btn_star);
 		builder2.setMessage("Are you sure you want to buy " + itemToPurchase.getName());
 		builder2.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener()
